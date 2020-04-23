2022bf7d8c0039e53bfe47563f1afd4743648aba
kbeckst3 <kellerjbeckstead@gmail.com>
2020-04-23 12:03:21 -0600

    adjusting project to java 8

--- src/module-info.java
+++
@@ -1,9 +0,0 @@
-module SoftwareOne {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-    opens org.openjfx to javafx.fxml;
-    opens controllers to javafx.fxml;
-    exports org.openjfx;
-}
--- src/org/openjfx/Main.java
+++ src/org/openjfx/Main.java
@@ -4,15 +4,16 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
 
 
 public class Main extends Application {
 
     public void start(Stage primaryStage) throws Exception{
-        // Load the font
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/mainScreen.fxml"));
 
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/mainScreen.fxml"));
         primaryStage.setScene(new Scene(root, 1005, 460));
         primaryStage.setResizable(false);
         primaryStage.show();
